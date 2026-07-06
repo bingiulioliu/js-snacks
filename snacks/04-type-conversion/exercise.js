4ae3a50ce1371a259ecf10dad59a4fce8105e3fe
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-07-06 11:43:31 +0200

    ADD es 5

--- snacks/04-type-conversion/exercise.js
+++ snacks/04-type-conversion/exercise.js
@@ -26,6 +26,9 @@ console.log(testoFiltrato);
 */
 
 // Scrivi qui le tue spiegazioni:
+// 1. 10 numero valido
+// 2. 10, 20 non è considerato un numero
+// 3. array sempre true perchè oggetto
 
 
 /* ESERCIZIO 3: GENERAZIONE DINAMICA (Array.from)
@@ -39,3 +42,60 @@ console.log(testoFiltrato);
 */
 
 // Scrivi qui il tuo codice:
+const numArr = Array.from({ length: 10 }, (_, i) => i + 1);
+console.log('Es 3');
+console.log(numArr);
+
+const potenza = (array) => {
+    return array.map((num => (
+        num % 2 === 0 ? 0 : num ** 3
+    )));
+}
+console.log(potenza(numArr));
+
+/* ESERCIZIO 4: TRASFORMAZIONE E RAGGRUPPAMENTO
+    Consegna:
+    1. Partendo dall'array 'prodotti', crea un nuovo array contenente solo gli oggetti
+        che hanno almeno 5 scorte.
+    2. Per questi prodotti, aggiungi una proprietà 'valoreTotale' (prezzo * scorte).
+    3. Infine, usa 'reduce' per calcolare la somma dei 'valoreTotale' di tutti questi prodotti.
+    
+    Output atteso:
+    Un numero (la somma del valore totale dei prodotti con scorte >= 5)
+*/
+
+// Scrivi qui il tuo codice:
+import { prodotti } from '../03-array-methods/index.js';
+const cinqueScorte = prodotti
+    .filter(prodotto => prodotto.scorte >= 5)
+    .map((p) => {
+        return {...p, valoreTotale: p.prezzo * p.scorte}
+    })
+    .reduce((acc, p) =>{
+        return acc + p.valoreTotale
+    }, 0)
+
+console.log('Es 5');
+console.log(cinqueScorte);
+
+/* ESERCIZIO 5: FILTRO E CREAZIONE DI UNA MAPPA (OGGETTO)
+    Consegna:
+    1. Crea un oggetto che "mappa" i nomi dei prodotti alle loro categorie.
+    2. Esempio: { "Tastiera": "informatica", "Mouse": "informatica", ... }
+    3. Usa 'reduce' per creare questo oggetto partendo dall'array 'prodotti'.
+    4. (Bonus) Fai la stessa cosa, ma includi nell'oggetto solo i prodotti
+        che costano più di 100 euro.
+*/
+
+// Scrivi qui il tuo codice:
+console.log('Es 5');
+
+const filteredProd = prodotti
+    .reduce((acc, p) => {
+        if (p.prezzo > 100) {
+            acc[p.nome] = p.categoria
+        }
+        return acc;
+    }, {})
+
+console.log(filteredProd);
